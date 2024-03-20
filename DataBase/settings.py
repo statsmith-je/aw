db94be80f4ac08dd8bd5086aa1c193c8059f788b
unknown <James Smith>
2024-03-19 18:46:54 -0700

    fix?

--- DataBase/settings.py
+++ DataBase/settings.py
@@ -121,5 +121,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import django_heroku
-django_heroku.settings(locals())
+import django_on_heroku
+django_on_heroku.settings(locals())
